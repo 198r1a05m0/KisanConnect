9f5cf1c3add7a4ee95ed951fc44a74721c9c416b
198r1a05m0 <198r1a05m0@gmail.com>
2024-12-19 22:55:42 +0530

    thirdcommit

--- src/App.js
+++ src/App.js
@@ -1,15 +1,10 @@
-
 import './App.css';
-import Button from './Components/Button';
+import Hero from './modules/component/Hero';
 
 function App() {
   return (
     <div className="App">
-      <h1 className='bg-orange-500'>KisanConnect</h1>
-      <div className='flex text-center justify-center gap-5 '>
-      <Button label="Post a job" className="bg-blue-600 hover:bg-green-600 p-8 border-2 border-cyan-500 rounded text-white"/>
-      <Button label="Find a job" className="bg-blue-600 hover:bg-green-600 p-8 border-2 border-cyan-500 rounded text-white"/>
-      </div>
+     <Hero />
     </div>
   );
 }
