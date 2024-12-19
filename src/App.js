547758133c1acdc9cfa446a06f6eb1bb933b2d48
AVINASH710-SIDAM <sidam.avinash@capgemini.com>
2024-12-19 15:27:27 +0530

    Your comment

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 
 import './App.css';
+import Button from './Components/Button';
 
 function App() {
   return (
     <div className="App">
       <h1>KC</h1>
       <p>helo world</p>
+      <Button label={"submit"} className="bg-red-400 hover:bg-red-800 p-8 text-white"/>
     </div>
   );
 }
